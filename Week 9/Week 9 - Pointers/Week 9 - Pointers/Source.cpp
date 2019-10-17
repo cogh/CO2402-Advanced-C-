d9847a60a7405d00320fcfd7a8d68a279c0009b9
Cogh <coghdevelops@gmail.com>
2019-10-17 11:35:13 +0100

    Buggered week 9 folder

--- Week 9/Week 9 - Pointers/Week 9 - Pointers/Source.cpp
+++ Week 9/Week 9 - Pointers/Week 9 - Pointers/Source.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <stdlib.h>
+#include <algorithm>
 
 using namespace std;
 
@@ -8,72 +10,30 @@ void change_int_pointer(int* param_int_pointer) {
 	*param_int_pointer = 10;
 }
 
-void PassByRefrance(int& var)
-{
-
+void cancel(int* param_int_pointer_1, int* param_int_pointer_2) {
+	int* smaller_pointer = min(param_int_pointer_1, param_int_pointer_2);
+	smaller_pointer = 0;
 }
-
-void FakePassByRefrance(int* pvar)
-{
-	int& var = *pvar;
-
-	var++;
-
-
-}
-
-
-//Spooky
-template<class T>
-void Spooky(T something)
-{
-	std::cout << something << std::endl;
-}
-
-
-template<class T, class W, class ...Args>
-void Spooky(T t, W w, Args... args)
-{
-	Spooky(t);
-	Spooky(w, args...);
-}
-
 
 
 int main() {
-
-
-	//Spooky(1);
-	//Spooky("tset");
-
-
-	Spooky(1, "asad", 3, "Hello wolrd");
-	//Spooky(1, "asdasd", 3);
-
-
-	system("pause");
-	exit(0);
 	// Exercise 1
-	/*int* int_pointer = new int(4);
+	int* int_pointer = new int(4);
 	cout << *int_pointer << endl;
 	change_int_pointer(int_pointer);
 	cout << *int_pointer << endl;
 	delete(int_pointer);
 
 	// Exercise 2
+	int* int_pointer_1;
+	int* int_pointer_2;
+	cout << "Int pointer 1: " << int_pointer_1;
+	cout << "Int pointer 2: " << int_pointer_2;
+	cancel(int_pointer_1, int_pointer_2);
+	cout << "Int pointer 1: " << int_pointer_1;
+	cout << "Int pointer 2: " << int_pointer_2;
+	cout << endl;
 
-	int a = 10;
-
-	PassByRefrance(a);
-	PassByRefrance(nullptr);
-
-
-	FakePassByRefrance(&a);
-	FakePassByRefrance(nullptr);
-
-
-
-	*/
 	// Stall
 	system("pause");
 }
